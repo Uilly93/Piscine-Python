f79c779af358efb02d1ecfa02dcd3c7526cde886
William Nocchi <wnocchi@f1r2s12.clusters.42paris.fr>
2025-10-09 10:34:27 +0200

    ex01 finished

--- day0/format_ft_time.py
+++ day0/format_ft_time.py
@@ -1,11 +1,21 @@
 
 import time
 
-start = time.time()
-date = time.asctime()
-ddate = time.strptime(date)
-# time.strptime()
-# litteraleDate = {ddate.}
-print(date)
+month = {1: "January",
+		 2: "February",
+		 3: "March",
+		 4: "April",
+		 5: "May",
+		 6: "June",
+		 7: "July",
+		 8: "August",
+		 9: "September",
+		 10: "October",
+		 11: "November",
+		 12: "December"}
 
-print(f'time = {start}')
+start = time.gmtime(0)
+date = time.asctime()
+
+print(f'Seconds since {month[start.tm_mon]} {start.tm_mday}, {start.tm_year}: {time.time()} or {time.time():e} in scientific notation')
+print(f'{date.split()[1]} {date.split()[2]} {date.split()[4]}')
