6ad50ce4c4d6f8b8f9477368b409920ec815a782
William Nocchi <wnocchi@f2r2s12.clusters.42paris.fr>
2025-10-13 15:13:33 +0200

    ex06 finished

--- day0/ex06/ft_filter.py
+++ day0/ex06/ft_filter.py
@@ -12,19 +12,19 @@ def ft_filter(func, array):
     return filtered
 
 
-def isMajor(x):
-    if x < 18:
-        return False
-    else:
-        return True
+# def isMajor(x):
+#     if x < 18:
+#         return False
+#     else:
+#         return True
 
 
-def main():
-    array = [34, 1, 40, 2, 33, 3, 56, 4, 18, 6, 15, 12, 11, 19, 27, 45]
-    print(f"before filter: {array}")
-    major = ft_filter(isMajor, array)
-    print(f"after filter: {major}")
+# def main():
+#     array = [34, 1, 40, 2, 33, 3, 56, 4, 18, 6, 15, 12, 11, 19, 27, 45]
+#     print(f"before filter: {array}")
+#     major = ft_filter(isMajor, array)
+#     print(f"after filter: {major}")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
