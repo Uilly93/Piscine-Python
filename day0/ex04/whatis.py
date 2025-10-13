91c2f6f6156f5e46b1e239e44f6a3bd1079a1e5d
William Nocchi <wnocchi@f2r2s12.clusters.42paris.fr>
2025-10-13 11:59:38 +0200

    ex05 finished

--- day0/ex04/whatis.py
+++ day0/ex04/whatis.py
@@ -1,15 +1,29 @@
 import sys
 
 
-def main():
+def whatIs(argv):
+    """
+    printing if a number is 'Odd' or 'Even'
+    raising 'AssertionError' exception if wrong argument is givent to the
+    function
+    """
     result = {0: "Even", 1: "Odd"}
-    if sys.argv.__len__() == 2:
-        arg = sys.argv[1]
+    if argv.__len__() == 2:
+        arg = argv[1]
         if arg.isdigit():
             num = int(arg)
             print(f'I\'m {result[num % 2]}')
-    elif sys.argv.__len__() > 2:
-        print('AssertionError: more than one argument is provided')
+        else:
+            raise AssertionError("Assertion Error: argument is not an integer")
+    elif argv.__len__() > 2:
+        raise AssertionError("Assertion Error: more than one argument is provided")
+
+
+def main():
+    try:
+        whatIs(sys.argv)
+    except AssertionError as e:
+        print(e)
 
 
 if __name__ == "__main__":
---
+++ day0/ex05/building.py
@@ -0,0 +1,46 @@
+import sys
+
+
+def countOccurence(argv: list[str]) -> dict:
+    if argv.__len__() > 2:
+        raise AssertionError("AssertionError: Too many arguments")
+    if argv.__len__() < 2:
+        raise AssertionError(
+            "AssertionError: You have to provide one argument")
+    chars = {"characters": 0,
+             "ponctuation": 0,
+             "upper": 0,
+             "digits": 0,
+             "spaces": 0,
+             "lower": 0, }
+    for c in argv[1]:
+        chars["characters"] += 1
+        if c.isdigit():
+            chars["digits"] += 1
+        elif not c.isalnum() and not c.isspace():
+            chars["ponctuation"] += 1
+        elif c.islower():
+            chars["lower"] += 1
+        elif c.isupper():
+            chars["upper"] += 1
+        elif c.isspace():
+            chars["spaces"] += 1
+    return chars
+
+
+def main():
+    argv = sys.argv
+    try:
+        chars = countOccurence(argv)
+        print(f'The text contains {chars["characters"]} characters:\
+                \n{chars["upper"]} upper letters\
+                \n{chars["lower"]} lower letters\
+                \n{chars["ponctuation"]} punctuation marks\
+                \n{chars["spaces"]} spaces\
+                \n{chars["digits"]} digits')
+    except AssertionError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
